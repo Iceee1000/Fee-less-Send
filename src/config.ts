65a8f64e684f9d0643bf97f1d678383d9bf558cb
Mekanical <jbornberg@gmail.com>
2022-04-12 07:38:17 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import {Connection, PublicKey, Keypair, Signer} from '@solana/web3.js';
 import base58 from 'bs58'
 
 export const TOKEN_FEE= .01;
-export const TOKEN_MINT_PUBKEY = new PublicKey('GV6n9Uow3XzMWSs8vwTCML8SvMA6ozbidaEfdPoSoraQ');
+export const TOKEN_MINT_PUBKEY = new PublicKey('some public key');
 
-const fee_payer_private_key='3WT5kx6C7N6fBfFwuNP9Mum3bjEtADsZUFBzdcqwAnkX4vSiMuNjAHY1eH51jjR2Tsv8m1mUf2Bz4jN8kqWaruBY';
+const fee_payer_private_key='process.env.privateKey';
 let feePayerKeypair: Keypair = Keypair.fromSecretKey(base58.decode(fee_payer_private_key));
 export const FEE_PAYER_KEYPAIR: Signer = feePayerKeypair;
 
